3a876dcf598c0cf47117a4e7a8b88286c56a68df
JanKaul <jankaul@mailbox.org>
2022-11-18 10:41:43 +0100

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import { match } from "ts-pattern";
+import { pattern, Result, variant } from "variant-ts";
+import { Action, Response } from "./common";
+
+export class FS {
+    fileType(path: String): Promise<String> {
+        return new Promise((resolve, reject) => {
+            let worker = new Worker(new URL("./worker/worker", import.meta.url), { "type": "module" });
+            worker.onmessage = <T, V>({ data }: { data: Result<T, V> }) => {
+                match(data)
+                    .with(pattern("ok"), res => {
+                        match(res.value as Response)
+                            .with(pattern("FileType"), res => { resolve(res.value) })
+                            .otherwise(_ => { reject("Wrong response type.") })
+                    })
+                    .with(pattern("err"), res => { reject(res.value) })
+                    .exhaustive()
+                worker.terminate()
+            };
+            worker.onerror = (e) => {
+                reject(e)
+            };
+            let action = variant<Action>("FileType", "/");
+            worker.postMessage(action);
+            return
+        })
+    }
+}
---
+++ src/worker/worker.ts
@@ -0,0 +1,28 @@
+import { default as FS } from '@isomorphic-git/lightning-fs';
+import { match } from 'ts-pattern';
+import { err, ok, pattern, variant, Variant } from "variant-ts";
+import { Action, Response } from "../common";
+
+
+
+let fs = new FS("lightningFS")
+
+let run = async (action: Action) => {
+    return match(action)
+        .with(pattern("FileType"), async res => {
+            let stats = await fs.promises.stat(res.value);
+            let file_type = stats.type
+            return ok(variant<Response>("FileType", file_type))
+        })
+        .exhaustive()
+}
+
+self.onmessage = ({ data: { action } }) => {
+    run(action).then(res => { self.postMessage(res) })
+};
+
+self.onerror = (e) => {
+    self.postMessage({
+        result: err(e),
+    });
+}
